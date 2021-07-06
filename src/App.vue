c305d2f798b039503228fe96a5b21042b06bda8b
yuyongjin <yuyongjin@antiy.cn>
2021-07-06 16:31:37 +0800

    添加nav菜单

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,53 @@
 <template>
   <div id="app">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
+      <el-menu-item index="/">首   页</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">JavaScript</el-menu-item>
+        <el-menu-item index="2-2">python</el-menu-item>
+        <el-submenu index="2-3">
+          <template slot="title">SQL</template>
+          <el-menu-item index="2-4-1">MongoDB</el-menu-item>
+          <el-menu-item index="2-4-2">MySQL</el-menu-item>
+          <el-menu-item index="2-4-3">PostGress</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3" >消息中心</el-menu-item>
+      <el-menu-item index="4"
+        >个人主页</el-menu-item
+      >
+    </el-menu>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/canvas">Canvas</router-link>
     </div>
-      <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
+  name: "App",
+  components: {},
+  data(){
+    return{
+       activeIndex: '1',
+       handleSelect(key, keyPath) {
+        console.log( key, keyPath);
+        this.$router.replace(key)
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 // 全局css
 import './assets/common.less';
 // 全局方法
 import "./util/util.js"
 Vue.config.productionTip = false
 
+
+Vue.use(ElementUI,{ size: '', zIndex: 2000 });
+
 new Vue({
   router,
-
   render: h => h(App)
 }).$mount('#app')
